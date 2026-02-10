af6e1c54647c690b4f859cbebaa7bde9a04c05c6
Manorath <chughmanorath@gmail.com>
2026-02-10 17:33:56 +0530

    doing1

--- Backend/src/lib/auth.ts
+++ Backend/src/lib/auth.ts
@@ -9,13 +9,19 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  socialProviders: {
+  socialProviders: {  
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
-  trustedOrigins: ["http://localhost:5173", "http://localhost:3000"],
+  trustedOrigins: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://manorath.me",
+    "http://www.manorath.me",
+    "http://13.222.109.240",
+  ],
   secret: process.env.BETTER_AUTH_SECRET!,
   baseURL: process.env.BETTER_AUTH_URL!,
   advanced: {
